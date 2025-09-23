f07b002ef7f0b0fc626fde2b2635cc020836b088
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-09-23 18:23:48 +0000

    en la pagina de login quita cualquier dado como correo y contraseña se e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ import { AuthContext } from '@/context/auth-context';
 import { Loader2 } from 'lucide-react';
 
 export default function LoginPage() {
-  const [email, setEmail] = React.useState('emilio20d2@gmail.com');
-  const [password, setPassword] = React.useState('123456');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
   const [isSubmitting, setIsSubmitting] = React.useState(false);
   
   const router = useRouter();
