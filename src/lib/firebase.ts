c73d7da144a7f058854f3e689c267b04a2610d13
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-17 09:17:21 +0000

    quiero que apartir de ahora se suban los cambios a github sin los datos

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyA4z_VJJhUe7Mo6q2a3xcT5b_1s8Y8cj6M",
-  authDomain: "studio-4902578709-bd4a7.firebaseapp.com",
-  projectId: "studio-4902578709-bd4a7",
-  storageBucket: "studio-4902578709-bd4a7.appspot.com",
-  messagingSenderId: "778074465222",
-  appId: "1:778074465222:web:9c57927ff0648908a348c7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
